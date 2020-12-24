cfbd2073be095e4c0ca8c1927bfc4f16b5b0aab3
AleksKislov <sinokislov@gmail.com>
2020-12-24 11:29:59 +0300

    Add info for landing page

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -20,6 +20,23 @@ const Landing = ({ isAuthenticated }) => {
               Войти
             </Link>
           </div>
+
+          <div className='card text-primary bg-light mt-3'>
+            <div className='card-header'>
+              <h5>Место, где вы сможете</h5>
+            </div>
+            <div className='card-body'>
+              <p className='card-text' style={{ marginRight: "2rem", textAlign: "left" }}>
+                <ul>
+                  <li>Почитать тексты и книги на китайском языке со встроенным переводом</li>
+                  <li>Изучать любые слова, добавляя их в личные списки для изучения</li>
+                  <li>Пользоваться словарем с анимированными иероглифами</li>
+                  <li>Проходить тесты на знание китайского языка</li>
+                  <li>И многое другое, так как проект каждый день развивается!</li>
+                </ul>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
