1a924c843336d5f0510950e87634630be3f5faa9
AleksKislov <sinokislov@gmail.com>
2020-12-27 00:18:16 +0300

    Change UX for counting read chars

--- src/components/texts/Paragraph.js
+++ src/components/texts/Paragraph.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import TippyTooltip from "../translation/TippyTooltip";
 import { v4 as uuid } from "uuid";
 import Tippy from "@tippyjs/react";
@@ -6,6 +6,7 @@ import { countZnChars } from "../../actions/helpers";
 
 const Paragraph = ({ chunk, translation, hideFlag, index, originTxt }) => {
   const numOfChars = countZnChars(originTxt);
+  const [alreadyRead, setAlreadyRead] = useState(false);
 
   const paragraphNum = (
     <Tippy content={<span>{"Параграф №"}</span>}>
@@ -15,8 +16,11 @@ const Paragraph = ({ chunk, translation, hideFlag, index, originTxt }) => {
 
   const paragraphPlus = (
     <Tippy content={<span>{`Прочитано ${numOfChars} 字`}</span>}>
-      <div className='paragraphPlus'>
-        <i className='fas fa-plus'></i>
+      <div
+        className={`paragraphToRead paragraph-${alreadyRead ? "minus" : "plus"}`}
+        onClick={() => setAlreadyRead(!alreadyRead)}
+      >
+        <i className={`fas fa-${alreadyRead ? "minus" : "plus"}-square`}></i>
       </div>
     </Tippy>
   );
@@ -24,7 +28,10 @@ const Paragraph = ({ chunk, translation, hideFlag, index, originTxt }) => {
   return (
     <Fragment>
       <div className={`result col-sm-${hideFlag ? "12" : "6"} my-1`}>
-        <div className='card border-primary alreadyRead' style={{ height: "100%" }}>
+        <div
+          className={`card border-primary ${alreadyRead && "alreadyRead"}`}
+          style={{ height: "100%" }}
+        >
           <p className='card-text textPadding'>
             {chunk.map(word => (
               <TippyTooltip word={word} key={uuid()} />
@@ -34,7 +41,7 @@ const Paragraph = ({ chunk, translation, hideFlag, index, originTxt }) => {
           {paragraphPlus}
         </div>
       </div>
-      <div className='col-sm-6 my-1' style={hideFlag ? hiddenStyle : {}}>
+      <div className='col-sm-6 my-1' style={hideFlag ? hidden : {}}>
         <div className='card border-primary' style={{ height: "100%" }}>
           <p className='card-text textPadding'>{translation}</p>
           {paragraphNum}
@@ -44,7 +51,7 @@ const Paragraph = ({ chunk, translation, hideFlag, index, originTxt }) => {
   );
 };
 
-const hiddenStyle = {
+const hidden = {
   display: "none"
 };
 
