91ee6739bfbc5125b1cb29faae04da52164c6927
AleksKislov <sinokislov@gmail.com>
2020-12-30 12:29:09 +0300

    Add gsap animation to paragraph component

--- src/components/texts/Paragraph.js
+++ src/components/texts/Paragraph.js
@@ -5,6 +5,7 @@ import Tippy from "@tippyjs/react";
 import { countZnChars } from "../../actions/helpers";
 import { connect } from "react-redux";
 import { readToday, unreadToday } from "../../actions/auth";
+import { TweenMax, Back } from "gsap";
 
 const Paragraph = ({
   chunk,
@@ -28,15 +29,40 @@ const Paragraph = ({
     }
   }, [user]);
 
-  const readOrUnread = async () => {
+  const readOrUnread = () => {
     if (alreadyRead) {
       setAlreadyRead(!alreadyRead);
       if (user) unreadToday({ num: numOfChars, path: window.location.pathname, ind: index });
     } else {
       setAlreadyRead(!alreadyRead);
-      if (user) readToday({ num: numOfChars, path: window.location.pathname, ind: index });
+      if (user) changeTeam();
     }
   };
+
+  function changeTeam() {
+    const box = document.querySelector("#box" + index);
+    box.style.display = "block";
+    const teamRed = document.querySelector(".progress");
+    const rect = box.getBoundingClientRect();
+    teamRed.appendChild(box);
+    TweenMax.set(box, { x: 0, y: 0 });
+    TweenMax.to(box, 1.2, { width: "0.9rem", height: "0.9rem", color: "#18BC9C", opacity: "0" });
+    const newRect = box.getBoundingClientRect();
+
+    TweenMax.from(box, 1, {
+      x: rect.left - newRect.left,
+      y: rect.top - newRect.top,
+      ease: Back.easeOut,
+      onComplete: fadeIt
+    });
+
+    function fadeIt() {
+      setTimeout(() => {
+        teamRed.removeChild(box);
+      }, 500);
+      readToday({ num: numOfChars, path: window.location.pathname, ind: index });
+    }
+  }
 
   const paragraphNum = (
     <Tippy content='Параграф №'>
@@ -50,6 +76,7 @@ const Paragraph = ({
         className={`paragraphToRead paragraph-${alreadyRead ? "minus" : "plus"}`}
         onClick={() => readOrUnread()}
       >
+        <div className='box align-middle center' id={"box" + index}></div>
         <i className={`fas fa-${alreadyRead ? "minus" : "plus"}-square`}></i>
       </div>
     </Tippy>
